c44e2c724a1adabb00ca55786610dc70a8d69d3a
Gogs <r2d2-lex@yandex.ru>
2023-05-02 16:35:05 +0500

    Обработка ошибок + отладочные сообщения

--- HPSwitch.py
+++ HPSwitch.py
@@ -2,12 +2,13 @@ import logging
 import requests
 import config
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 OPERATION_STATUS_UPLOAD_RESPONSE = 'successful'
 ERROR_MESSAGES_UPLOAD_RESPONSE = 'errorMsgs'
 CPU_STRING = 'id="cpu_util_prog_bar_val">'
 MEMORY_STRING = 'id="mem_util_prog_bar_val">'
+TRANSFER_STATUS = 'xferStatus'
 
 url = 'http://' + config.COMMUTATOR_ADDRESS
 url_login = '/htdocs/login/login.lua'
@@ -30,26 +31,42 @@ def upload_to_tftp(tftp_address, config_filename):
     })
 
     if login():
-        file_upload_result = session.post(url + url_file_transfer_tftp, data=file_transfer_fields)
-        print(f'Status code: {file_upload_result.status_code}')
-        if file_upload_result.status_code == 200:
-            result_json = file_upload_result.json()
-            logging.info(result_json)
-            if OPERATION_STATUS_UPLOAD_RESPONSE in result_json:
-                operation_status = bool(result_json[OPERATION_STATUS_UPLOAD_RESPONSE])
-                if operation_status:
-                    logging.info(f'Successfully backup config: {config_filename}')
+        try:
+            file_upload_result = session.post(url + url_file_transfer_tftp, data=file_transfer_fields)
+            print(f'Status code of url {url + url_file_transfer_tftp}: {file_upload_result.status_code}')
+            if file_upload_result.status_code == 200:
 
-                    file_transfer_status = session.get(url + url_status_file_transfer_tftp)
-                    logging.info(f'Status code: {file_transfer_status.status_code}')
-                    if file_transfer_status.status_code == 200:
-                        status_json = file_transfer_status.json()
-                        logging.info(status_json)
+                try:
+                    result_json = file_upload_result.json()
+                    logging.debug(result_json)
+                    if OPERATION_STATUS_UPLOAD_RESPONSE in result_json:
+                        operation_status = bool(result_json[OPERATION_STATUS_UPLOAD_RESPONSE])
+                        if operation_status:
+                            logging.info(f'Successfully backup config: {config_filename}')
 
-                else:
-                    logging.info(f'Error backup config: {result_json[ERROR_MESSAGES_UPLOAD_RESPONSE]}')
+                            file_transfer_status = session.get(url + url_status_file_transfer_tftp)
+                            logging.debug(f'Status code of url {url + url_status_file_transfer_tftp} : {file_transfer_status.status_code}')
+                            if file_transfer_status.status_code == 200:
+                                status_json = file_transfer_status.json()
+                                logging.debug(f'status_json: {status_json}')
+                                # Success upload:
+                                # {'xferCodeStatus': 0, 'xferDownloadResult': 0, 'statusReadError': False, 'xferInProgress': 0, 'progressVal': 100, 'xferMessage': '', 'xferStatus': 10}
+                                # Progress uload:
+                                # {'xferCodeStatus': 0, 'xferDownloadResult': 0, 'statusReadError': False, 'xferInProgress': 1, 'progressVal': 0, 'xferMessage': 'TFTP Config transfer starting...', 'xferStatus': 1}
+                                if TRANSFER_STATUS in status_json:
+                                    result = str(status_json[TRANSFER_STATUS])
+                                    logging.info(f'Результат передачи: {result}')
+                                    return result
+                        else:
+                            logging.info(f'Error backup config: {result_json[ERROR_MESSAGES_UPLOAD_RESPONSE]}')
+                except (ValueError, KeyError):
+                    logging.info('Ошибка данных')
+
+        except requests.RequestException:
+            logging.info('Ошибка сети')
+
     else:
-        logging.info("login failed")
+        logging.info("Login failed...")
     logout()
     return result
 
@@ -95,5 +112,9 @@ def logout():
 
 
 if __name__ == "__main__":
-    get_mem()
-    # upload_to_tftp(config.TFTP_ADDRESS, 'test123.cfg')
+    config_name = 'test123.cfg'
+    if upload_to_tftp(config.TFTP_ADDRESS, config_name):
+        message = f'Success upload config {config_name} on {config.TFTP_ADDRESS}'
+    else:
+        message = f'Error upload config {config_name} on {config.TFTP_ADDRESS}'
+    print(message)
